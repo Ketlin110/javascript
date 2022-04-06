d2777e2ae20ccaed7c3e54cf54a27bfb8ade359e
Ketlin Souza <ketlinsouza110@gmail.com>
2022-04-06 11:18:17 -0300

    transformando objeto em JSON

---
+++ Projeto/treino/json.stringify.js
@@ -0,0 +1,5 @@
+let pessoa = {nome:'ketlin', idade:22, algo: null};
+let pessoaString = JSON.stringify(pessoa);
+console.log(pessoaString);
+
+// transformando um objeto em formato de JSON.
--- Projeto/treino/jsonparse.js
+++
@@ -1,7 +0,0 @@
-
-
-let pessoa = JSON.parse('{"Nome": "ketlin", "idade":90}');
-
-// Parse é utilizado para transformar a string em um objeto real.
-
-

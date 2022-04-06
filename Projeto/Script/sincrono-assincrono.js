f3b22157b2ca667c5c79939a1c21d9f5a0668ca0
Ketlin Souza <ketlinsouza110@gmail.com>
2022-04-06 14:04:00 -0300

    função síncrona e assíncrona

---
+++ Projeto/Script/sincrono-assincrono.js
@@ -0,0 +1,14 @@
+// Código SÍNCRONO
+
+let nome = 'ketlin';
+let sobrenome = 'souza';
+let completo = nome+' '+sobrenome;
+//executa uma função por vez, obtendo resposta para dar saquência na compilação.
+
+// Código ASSÍNCRONO
+
+let name = 'ketlin';
+let ela = 'ketlin';
+let temperatura = Maquininha.pegarTemperatura(); //ASSINCRONA
+let result = nome+''+sobrenome;
+// executa as demais funções independente de obter uma resposta ou não.

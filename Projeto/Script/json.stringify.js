74d9a3bd6f2bd71c0392778b0a7cf36b1a390634
Ketlin Souza <ketlinsouza110@gmail.com>
2022-04-06 12:09:54 -0300

    json stringify, json parse, e switch case

---
+++ Projeto/Script/json.stringify.js
@@ -0,0 +1,5 @@
+let pessoa = {nome:'ketlin', idade:22, algo: null, status:'on', dia:'06'};
+let pessoaString = JSON.stringify(pessoa);
+console.log(pessoaString);
+
+// transformando um objeto em formato de JSON.
---
+++ Projeto/Script/switch-case.js
@@ -0,0 +1,32 @@
+let dia = 3;
+let diaNome = '';
+
+switch (dia) {
+    case 1:
+        diaNome = 'Segunda-Feira';
+        break;
+    case 2:
+        diaNome = 'Terça-Feira';
+        break;
+    case 3:
+        diaNome = 'Quarta-Feira';
+
+    case 4:
+        diaNome = 'Quinta-Feira';
+        break;
+    case 5:
+        diaNome = 'Sexta-Feira';
+
+    case 6:
+        diaNome = 'Sabado';
+        break;
+    case 7:
+        diaNome = 'Domingo';
+    
+    case 8:
+        diaNome = 'teste';
+
+}
+
+document.getElementById("dia").innerHTML = "Hoje é:" + diaNome;
+console.log(diaNome);

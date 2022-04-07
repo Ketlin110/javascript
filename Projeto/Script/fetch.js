0d75f5dc1b57c1505c2d6f29ea077f007da2dfd7
Ketlin Souza <ketlinsouza110@gmail.com>
2022-04-07 13:40:59 -0300

    utilizando fetch para requisições

---
+++ Projeto/Script/fetch.js
@@ -0,0 +1,15 @@
+//fetch retorna uma promise
+function loadPosts(){ 
+  document.getElementById("posts").innerHTML = 'Carregando . . . ';
+
+  fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(function(resultado){
+         return resultado.json();
+      })
+      .then(function(json){
+         document.getElementById("posts").innerHTML = json.length+ ' posts';
+      })
+      .catch(function(error){
+        console.log("deu erro!");
+      })
+}

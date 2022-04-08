16df6bbbcd1bc2e9ec8f96a5e00b7aad2d4046b6
Ketlin Souza <ketlinsouza110@gmail.com>
2022-04-08 14:58:42 -0300

    correção de código/enviando imagens

--- Projeto/Script/Projeto-pizza/script.js
+++ Projeto/Script/Projeto-pizza/script.js
@@ -5,7 +5,7 @@ pizzaJson.map((item, index)=>{
    let pizzaItem = c('.models .pizza-item').cloneNode(true); // clonando pizza-item
    //preencher informações pizzaitem
  
-   pizzaItem.querySelector('.pizza-item--img').src = item.img;
+   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
